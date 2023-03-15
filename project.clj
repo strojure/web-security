2504409358f42bf7a1106e81e69aec5269cd63de
Sergey Trofimov <sergey@trofimov.name>
2023-03-15 17:18:22 +0100

    (chore): bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/web-security "1.0.0-28"
+(defproject com.github.strojure/web-security "1.0.1-29-SNAPSHOT"
   :description "Decoupled web security implementations for Clojure."
   :url "https://github.com/strojure/web-security"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
